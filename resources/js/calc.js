ce8b8797d9acf4dd21ddc8167d89ef95f87f29c4
Kevin Biddle <kdbiddle08@gmail.com>
2016-01-28 10:10:36 -0500

    Constructing logic

--- resources/js/calc.js
+++ resources/js/calc.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+    var numberInput = [];
+    var operation;
+
+    function addNumbers(a,b) {
+        return a+b;
+    }
+
+    function subtractNumbers(a,b) {
+        return a-b;
+    }
+
+    function multiplyNumbers(a,b){
+        return a*b;
+    };
+
+    function divideNumbers(a,b) {
+        return a/b;
+    }
+
+
     $(".btn").click(function() {
         $("#display").text($(this).val());
     });
